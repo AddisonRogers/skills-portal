fba459f7d460ebdbd6ce66bfc24c266174015516
Addison.Rogers <addison.rogers@fsp.co>
2025-07-08 10:13:10 +0100

    update: switch database configuration to Azure, update pnpm dependencies, and improve workspace files

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,41 @@
 import { betterAuth } from "better-auth";
-import { Pool } from "pg";
+import { MssqlDialect } from "kysely";
+import * as Tedious from 'tedious'
+import * as Tarn from 'tarn'
+
+const dialect = new MssqlDialect({
+  tarn: {
+    ...Tarn,
+    options: {
+      min: 0,
+      max: 10,
+    },
+  },
+  tedious: {
+    ...Tedious,
+    connectionFactory: () => new Tedious.Connection({
+      authentication: {
+        options: {
+          password: 'password',
+          userName: 'username',
+        },
+        type: 'default',
+      },
+      options: {
+        database: 'some_db',
+        port: 1433,
+        trustServerCertificate: true,
+      },
+      server: 'localhost',
+    }),
+  },
+})
 
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.DATABASE_URL,
-  }),
+  database: {
+    dialect,
+    type: "mssql"
+  },
   socialProviders: {
     microsoft: {
       clientId: process.env.MICROSOFT_CLIENT_ID as string,
