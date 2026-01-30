c24642bdab34fbefc61d896e84105636e0047c93
dotracc <addisonrogers@protonmail.com>
2026-01-30 23:59:10 +0000

    config changes

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,9 +1,7 @@
-import { execSync } from "node:child_process";
-import path from "node:path";
+import { execSync } from 'node:child_process';
+import path from 'node:path';
 
 export default async function globalSetup() {
-
-  execSync("npx dotenv -e .env.test -- npm run migrate", { stdio: "inherit" });
-
-  execSync("npx dotenv -e .env.test -- npm run seed:e2e", { stdio: "inherit" });
+  execSync('npx dotenv -e .env.test -- npm run migrate', { stdio: 'inherit' });
+  execSync('npx dotenv -e .env.test -- npm run seed:e2e', { stdio: 'inherit' });
 }
