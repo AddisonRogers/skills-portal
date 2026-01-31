1416407cf656b1dad40153598296af2e8d77871a
dotracc <addisonrogers@protonmail.com>
2026-01-31 21:01:15 +0000

    updating navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,46 +1,45 @@
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import AvatarOrLogin from "@/components/navbar/avatarOrLogin/avatarOrLogin";
-import { ThemeToggle } from "../ThemeToggle";
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import AvatarOrLogin from '@/components/navbar/avatarOrLogin/avatarOrLogin';
+import { ThemeToggle } from '../ThemeToggle';
 
 const navLinks = [
-	{ name: "Home", href: "/" },
-	{ name: "Learn", href: "/learn" },
-	{ name: "Skills", href: "/skills" },
-	{ name: "Projects", href: "/projects" },
-	{ name: "People", href: "/people" },
-	{ name: "Profile", href: "/profile" },
-	{ name: "AI", href: "/ai" },
-	{ name: "Admin Panel", href: "/admin" },
+  { name: 'Home', href: '/' },
+  { name: 'Learn', href: '/learn' },
+  { name: 'Skills', href: '/skills' },
+  { name: 'Projects', href: '/projects' },
+  { name: 'People', href: '/people' },
+  { name: 'Profile', href: '/profile' },
+  { name: 'AI', href: '/ai' },
+  { name: 'Admin Panel', href: '/admin' },
 ];
 
 export default async function Navbar() {
-	return (
-		<nav className="flex w-full px-4 bg-muted backdrop-blur border-b border-gray-200 shadow-sm sticky justify-between items-center">
-			<div className="flex items-start justify-baseline">
-				{navLinks.map((link) => (
-					<Link
-						key={link.name}
-						href={link.href}
-						className={cn(
-							"inline-block px-4 py-2 font-medium transition-colors duration-150 relative",
-							"hover:text-primary hover:font-semibold" +
-								" focus:outline-none focus-visible:ring-2 focus-visible:ring-primary/30",
-							"after:content-[''] after:block after:h-[2px] after:bg-primary after:scale-x-0 after:transition-transform after:duration-200 after:absolute after:left-0 after:right-0 after:-bottom-1 hover:after:scale-x-100",
-						)}
-					>
-						{link.name}
-					</Link>
-				))}
-			</div>
+  return (
+    <nav className="flex w-full px-4 bg-muted border-b border-gray-200 shadow-sm sticky justify-between items-center py-2 font-medium">
+      <div className="flex gap-6">
+        {navLinks.map((link) => (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={cn(
+              'transition-colors duration-150 relative',
+              'hover:text-primary hover:font-semibold' +
+                ' focus:outline-none focus-visible:ring-2 focus-visible:ring-primary/30',
+              "after:content-[''] after:block after:h-0.5 after:bg-primary after:scale-x-0 " +
+                'after:transition-transform after:duration-200 after:absolute after:left-0 ' +
+                'after:right-0 after:-bottom-1 hover:after:scale-x-100',
+            )}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
 
-			<div className="inline-block">
-				<ThemeToggle />
-			</div>
-
-			<div className="inline-block px-4 py-2 font-medium text-black mt-1">
-				<AvatarOrLogin />
-			</div>
-		</nav>
-	);
+      <div className="flex flex-row gap-4">
+        <ThemeToggle />
+        <AvatarOrLogin />
+      </div>
+    </nav>
+  );
 }
