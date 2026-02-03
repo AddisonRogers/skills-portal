d47f516b7410bfc41cf87bcfaafcd95df2b42e9c
HarrisonJ05 <harrisonjeffreys101@gmail.com>
2026-02-03 18:10:42 +0000

    fix: ensure ai/page is still a module

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,20 +1,20 @@
-// "use client";
+"use client";
 
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ErrorProvider } from "@/app/ai/providers/errorProvider.tsx";
 // import { McpProvider } from "@/app/ai/providers/mcpProvider.tsx";
 // import Chat from "@/app/ai/chat";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-// export default function AiPage() {
-// 	return (
-// 		<QueryClientProvider client={queryClient}>
-// 			<ErrorProvider>
-// 				<McpProvider>
-// 					<Chat />
-// 				</McpProvider>
-// 			</ErrorProvider>
-// 		</QueryClientProvider>
-// 	);
-// }
+export default function AiPage() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{/* <ErrorProvider>
+				<McpProvider>
+					<Chat />
+				</McpProvider>
+			</ErrorProvider> */}
+		</QueryClientProvider>
+	);
+}
